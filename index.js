b0c423c741a13211628b5fea0d37259dfd4a054f
vazquezpmarcos <vazquezpmarcos@gmail.com>
2022-05-24 13:22:41 +0200

    const

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function contarLetras(str, char) {
-    var repeticiones = 0;
-    for (var posicion = 0; posicion < str.length; posicion++) 
+    const repeticiones = 0;
+    for (const posicion = 0; posicion < str.length; posicion++) 
     {
-      if(str.charAt(posicion) == char) 
+      if(str.charAt(posicion) === char) 
         repeticiones += 1;
       }
     return repeticiones;
